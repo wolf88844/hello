ae68fcf520e579db76947ec2f8adca28f8938ecd
wolf88844 <wolf88840@163.com>
2024-12-30 15:15:58 +0800

    init web

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+
+    println!("Request: {:#?}", http_request);
+
+    let request_line = http_request.first().unwrap();
+
+    let (status_line,filname) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    } else  {
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n","404.html")
+    };
+
+    let contents = fs::read_to_string(filname).unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line, length, contents
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+}
